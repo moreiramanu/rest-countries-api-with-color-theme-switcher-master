404afa8e17e259dedb920d20bccad6ecd8d4c59b
moreiramanu <moreiraemanuely289@gmail.com>
2025-07-29 15:22:47 -0300

    correção \de erros

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function renderCountries(data) {
   data.forEach((country) => {
     const countryCard = document.createElement('a');
     countryCard.classList.add('country-card');
-    countryCard.href = `/paises.html?name=${country.name.common}`;
+    countryCard.href = `paises.html?name=${country.name.common}`;
     countryCard.innerHTML = `
       <img src="${country.flags.svg}" alt="${country.name.common} flag" />
       <div class="card-text">
